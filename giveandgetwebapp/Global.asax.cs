9ee17891db9da6bfd97d279e39963fa18bbd04f7
binhtran <trananbinh18@gmail.com>
2020-05-02 17:50:31 +0700

    Add scheduling job

--- giveandgetwebapp/Global.asax.cs
+++ giveandgetwebapp/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentScheduler;
+using giveandgetwebapp.Job;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,11 @@ namespace giveandgetwebapp
     {
         protected void Application_Start()
         {
+            //Job scheduling
+            var jobRegistry = new Registry();
+            jobRegistry.Schedule<JobCheckOutOfDate>().ToRunNow().AndEvery(1).Hours();
+            JobManager.Initialize(jobRegistry);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ giveandgetwebapp/Job/JobCheckOutOfDate.cs
@@ -0,0 +1,35 @@
+﻿using FluentScheduler;
+using giveandgetwebapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace giveandgetwebapp.Job
+{
+    public class JobCheckOutOfDate : IJob
+    {
+        public void Execute()
+        {
+            cap22t6Entities dbEntity = new cap22t6Entities();
+            Array arrPostOutOfDate = dbEntity.Posts.Where(x => x.ExpireDate <= DateTime.Now).ToArray();
+            foreach (Post post in arrPostOutOfDate) {
+                post.Status = 2;
+                Notification notification = new Notification();
+                notification.PostId = post.Id;
+                notification.UserId = post.Actor ?? default(int);
+                notification.Status = 1;
+                notification.CreateDate = DateTime.Now;
+                notification.Title = "Bài viết "+post.Title+" đã hết hạn";
+                notification.Contents = "Bài viết " + post.Title + " đã hết hạn, click vào để cho đồ";
+                notification.Type = 2;
+
+                dbEntity.Notifications.Add(notification);
+            }
+
+            dbEntity.SaveChanges();
+
+                
+        }
+    }
+}
